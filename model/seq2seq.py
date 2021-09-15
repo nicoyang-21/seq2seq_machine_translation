fce27c99c5b561900849f0e4a3e4d723b3133848
nico_yang@126.com <ycy210921>
2021-09-15 14:44:04 +0800

    changes

--- model/seq2seq.py
+++ model/seq2seq.py
@@ -2,3 +2,55 @@
 组件模型
 """
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from data_preprocess.data_process import USE_CUDA
+
+
+class EnconderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, n_layers=1):
+        super(EnconderRNN, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.rnn = nn.RNN(hidden_size, hidden_size, n_layers)
+
+    def forward(self, word_inputs, hidden):
+        seq_len = len(word_inputs)
+        embedded = self.embedding(word_inputs).view(seq_len, 1, -1)
+        output, hidden = self.rnn(embedded, hidden)
+        return output, hidden
+
+    def init_hidden(self):
+        hidden = torch.zeros(self.n_layers, 1, self.hidden_size)
+        if USE_CUDA:
+            hidden = hidden.cuda()
+        return hidden
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, n_layer=1, dropout_p=0.1):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layer
+        self.dropout_p = dropout_p
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.rnn = nn.RNN(hidden_size, hidden_size, n_layer, dropout=dropout_p)
+        self.out = nn.Linear(hidden_size, output_size)
+
+    def forward(self, word_input, last_hidden):
+        word_embedded = self.embedding(word_input).view(1, 1, -1)
+        rnn_output, hidden = self.rnn(word_embedded, last_hidden)
+
+        rnn_output = rnn_output.squeeze(0)
+        output = F.log_softmax(self.out(rnn_output))
+
+        return output, hidden
+
---
+++ train/train_function.py
@@ -0,0 +1,70 @@
+import random
+
+import torch
+
+from data_preprocess.data_process import MAX_LENGTH, sos_token, USE_CUDA, eos_token
+
+teacher_forcing_ratio = 0.5
+clip = 0.5
+
+
+def train(input_variable, target_variable, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion,
+          max_length=MAX_LENGTH):
+    """
+    Zero gradients of both optimizers
+    :param input_variable: 输入张量
+    :param target_variable: 目标词张量
+    :param encoder: EnconderRNN
+    :param decoder: DecoderRNN
+    :param encoder_optimizer:
+    :param decoder_optimizer:
+    :param criterion:
+    :param max_length:
+    :return:
+    """
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+    loss = 0  # Added onto for each word
+
+    # 获取输入和目标句子长度
+    input_length = input_variable.size()[0]
+    target_length = target_variable.size()[0]
+
+    # 通过encoder运行输入
+    encoder_hidden = encoder.inin_hidden()
+    encoder_outputs, encoder_hidden = encoder(input_variable, encoder_hidden)
+
+    # 准备输入和输出张量
+    decoder_input = torch.LongTensor([[sos_token]])
+    decoder_hidden = encoder_hidden    # 使用encoder的最后的hidden做为decoder初始hidden
+    if USE_CUDA:
+        decoder_input = decoder_input.cuda()
+
+    # 选择是否使用教师指导
+    use_teacher_forcing = random.random() < teacher_forcing_ratio
+    if use_teacher_forcing:     # 使用真实的目标此作为输入
+        for di in range(target_length):
+            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+            loss += criterion(decoder_output, target_variable[di])
+            decoder_input = target_variable[di]     # 目标的下一个输入词
+
+    else:    # 使用网络的预测输出作为下一时刻的输出
+        for di in range(target_length):
+            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+            loss += criterion(decoder_output, target_variable[di])
+            topv, topi = decoder_output.data.topk(1)  # 获取输出最可能结果的index
+            ni = topi[0][0]
+
+            decoder_input = torch.LongTensor([[ni]])    # 作为下一时刻的输入
+            if USE_CUDA:
+                decoder_input = decoder_input.cuda()
+
+            if ni == eos_token:
+                break
+    loss.backward()
+    torch.nn.utils.clip_grad_norm(encoder.parameters(), clip)
+    torch.nn.utils.clip_grad_norm(decoder.parameters(), clip)
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.item() / target_length
